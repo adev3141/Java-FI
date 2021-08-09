6658cce647770a9f5d176e13dfccffd19754ca94
alihassanoo <57753201+alihassanoo@users.noreply.github.com>
2021-08-09 19:21:07 +1000

    ExceptionHandling Practice
    Plural site practice

---
+++ src/com/company/ExceptionHandling.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class ExceptionHandling {
+
+    public static  void main(String args[]){
+        int i = 12;
+        int j = 2;
+        try{
+            int result = i/(j-2);
+            System.out.println(result);
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+            ex.printStackTrace();
+        }
+    }
+
+}
