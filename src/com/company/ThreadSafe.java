640111f3eb280e427d82b70f687a72b6f7c25d67
alihassanoo <57753201+alihassanoo@users.noreply.github.com>
2021-08-10 19:43:25 +1000

    Threadsafe Understanding
    Understanding the difference between synchronized and non synchronized classes in JAVA

---
+++ src/com/company/ThreadSafe.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class ThreadSafe {
+    //a thread safe object is that which is a blueprint of a synchronized class
+    // two threads cant change its state or properties
+    //only one thread can
+    //by default JAVA COLLECTIONS CLASS is non synchronized meaning they are not thread safe
+}
